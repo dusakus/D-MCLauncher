f222c8b8603805ebbdcac198ee84219a3162b031
dusakus <>
2013-03-27 14:19:00 +0000

    Project INIT

---
+++ DMCLgit/src/dmclgit/DMCLgit.java
@@ -0,0 +1,13 @@
+package dmclgit;
+
+/**
+ *
+ * @author dusakus
+ */
+public class DMCLgit {
+
+    //@param args the command line arguments
+    public static void main(String[] args) {
+        // TODO Do all the things !!!
+    }
+}
